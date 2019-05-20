86f1db46f005fcca40eeb3db6149e3db7987b542
Michelle Moon <michellemoon236@gmail.com>
2019-05-20 19:17:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @id = id
     @name = name
     @grade = grade
@@ -49,7 +49,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    self.new(row[1], row[2], row[0])
+    self.new(row[0], row[1], row[2])
   end
   
   def self.find_by_name(name)
